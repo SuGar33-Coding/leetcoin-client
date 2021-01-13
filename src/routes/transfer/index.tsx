d95cdc57ec3140d6a3189a5d4985efd5edcaff75
Gabriel Klavans <gabeklav@bu.edu>
2021-01-13 01:42:45 -0500

    Fleshed out transfer a bit. Doesn't do anything on confirm yet

--- src/routes/transfer/index.tsx
+++ src/routes/transfer/index.tsx
@@ -4,20 +4,47 @@ import UserSelect from "../../components/user-select";
 import style from "./style.css";
 
 const Transfer: FunctionalComponent = () => {
-    const [inputValue, setInputValue] = useState<string>("");
+    const [userValue, setUserValue] = useState<string>("");
+    const [amountValue, setAmountValue] = useState<number>(0);
     const [isValidUser, setIsValidUser] = useState<boolean>(false);
 
-    const handleInputChange = (selectedInput: string, isUser: boolean) => {
-        setInputValue(selectedInput);
+    const handleUserInputChange = (selectedInput: string, isUser: boolean) => {
+        setUserValue(selectedInput);
         setIsValidUser(isUser);
+    };
+
+    const handleAmountInputChange = (event: any) => {
+        const amt = event.target.value;
+        setAmountValue(amt);
+    };
+
+    const handleTransferSubmit = (event: any) => {
+        alert(`Test: Sent ${amountValue} LC to ${userValue}! 😊`);
     };
 
     return (
         <div class={style.transfer}>
             <h1>Transfer LeetCoin! 😳</h1>
-            <p>Current Value: {inputValue}</p>
-            <p>Is valid user? {isValidUser ? "Yes" : "No"}</p>
-            <UserSelect onSelectOption={handleInputChange} />
+            <label>User:</label>
+            <br />
+            <UserSelect onSelectOption={handleUserInputChange} />
+            <br />
+            <form onSubmit={handleTransferSubmit}>
+                <label>Amount:</label>
+                <br />
+                <input
+                    type="number"
+                    step="any"
+                    onInput={handleAmountInputChange}
+                />
+                <br />
+                <br />
+                <input
+                    type="submit"
+                    value="Confirm Transfer"
+                    disabled={!isValidUser || !(amountValue > 0)}
+                />
+            </form>
         </div>
     );
 };
