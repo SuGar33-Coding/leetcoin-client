adbd8c43ac1b5da983636861df15ebfd198f39f5
Gabriel Klavans <gabeklav@bu.edu>
2021-01-23 01:03:32 -0500

    Disable transaction page

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,11 @@
 import { FunctionalComponent, h } from "preact";
 import { Route, Router } from "preact-router";
 
+// import Transaction from "../routes/transaction";
 import Transactions from "../routes/transactions";
 import Profile from "../routes/profile";
 import NotFoundPage from "../routes/notfound";
 import Header from "./header";
-import Transaction from "../routes/transaction";
 import Transfer from "../routes/transfer";
 import Payment from "../routes/payment";
 import Earnings from "../routes/earnings";
@@ -20,7 +20,7 @@ const App: FunctionalComponent = () => {
             <Router>
                 <Route path="/" component={Transactions} />
                 <Route path="/profile/" component={Profile} />
-                <Route path="/transaction" component={Transaction} />
+                {/* <Route path="/transaction" component={Transaction} /> */}
                 <Route path="/transfer" component={Transfer} />
                 <Route path="/payment" component={Payment} />
                 <Route path="/earnings/:type" component={Earnings} />
