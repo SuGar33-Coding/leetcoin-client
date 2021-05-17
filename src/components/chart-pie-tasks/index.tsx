56a5cdbd0087c372e56df92e4294ab7f67d8df0a
Gabriel Klavans <gabeklav@bu.edu>
2021-05-17 17:15:08 -0400

    Started the stacked chart!

--- src/components/chart-pie-tasks/index.tsx
+++ src/components/chart-pie-tasks/index.tsx
@@ -4,86 +4,50 @@ import {
 	AreaChart,
 	CartesianGrid,
 	Legend,
-	Line,
-	LineChart,
 	ResponsiveContainer,
 	Tooltip,
 	XAxis,
 	YAxis
 } from "recharts";
-import { Api } from "../../utils/api";
 
-const ChartPieTasks: FunctionalComponent = () => {
-	const data = [
-		{
-			name: "Page A",
-			uv: 4000,
-			pv: 2400,
-			amt: 2400
-		},
-		{
-			name: "Page B",
-			uv: 3000,
-			pv: 1398,
-			amt: 2210
-		},
-		{
-			name: "Page C",
-			uv: 2000,
-			pv: 9800,
-			amt: 2290
-		},
-		{
-			name: "Page D",
-			uv: 2780,
-			pv: 3908,
-			amt: 2000
-		},
-		{
-			name: "Page E",
-			uv: 1890,
-			pv: 4800,
-			amt: 2181
-		},
-		{
-			name: "Page F",
-			uv: 2390,
-			pv: 3800,
-			amt: 2500
-		},
-		{
-			name: "Page G",
-			uv: 3490,
-			pv: 4300,
-			amt: 2100
-		}
-	];
+interface Props {
+	data: any;
+}
 
+const ChartPieTasks: FunctionalComponent<Props> = props => {
 	return (
 		<div className="chart-container" style={{ width: "100%", height: 300 }}>
 			<ResponsiveContainer>
-				<LineChart
-					data={data}
+				<AreaChart
+					// width={500}
+					// height={400}
+					data={props.data}
 					margin={{
-						top: 5,
+						top: 10,
 						right: 30,
-						left: 20,
-						bottom: 5
+						left: 0,
+						bottom: 0
 					}}
 				>
 					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="name" tick={{ fill: "white" }} />
-					<YAxis tick={{ fill: "white" }} />
+					<XAxis dataKey="date" tick={{ fill: "#FFF" }} />
+					<YAxis />
 					<Tooltip contentStyle={{ color: "#000" }} />
-					<Legend />
-					<Line
+					<Area
 						type="monotone"
-						dataKey="pv"
+						dataKey="trash"
+						stackId="1"
 						stroke="#8884d8"
-						activeDot={{ r: 8 }}
+						fill="#8884d8"
 					/>
-					<Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-				</LineChart>
+					<Area
+						type="monotone"
+						dataKey="dishes"
+						stackId="1"
+						stroke="#82ca9d"
+						fill="#82ca9d"
+					/>
+				</AreaChart>
 			</ResponsiveContainer>
 			<Legend />
 		</div>
--- src/routes/data/index.tsx
+++ src/routes/data/index.tsx
@@ -4,9 +4,22 @@ import ChartPieTasks from "../../components/chart-pie-tasks";
 import style from "./style.css";
 
 const Data: FunctionalComponent = () => {
+	const data = [
+		{
+			date: "05-01",
+			trash: 100,
+			dishes: 50
+		},
+		{
+			date: "05-02",
+			trash: 400,
+			dishes: 75
+		}
+	];
+
 	return (
 		<div class={style.data}>
-			<ChartPieTasks />
+			<ChartPieTasks data={data} />
 		</div>
 	);
 };
